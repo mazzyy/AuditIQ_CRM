e618648b8513dcaaee1ccc576b06977ac86ce4a0
uk14apyc <asheerali1997@gmail.com>
2025-05-02 01:38:30 +0200

    Refactor code structure for improved readability and maintainability

--- frontend/src/App_original.tsx
+++ frontend/src/App_original.tsx
@@ -15,7 +15,7 @@ import * as agentService from "./services/agentService";
 import * as productService from "./services/productService";
 import * as customerService from "./services/customerService";
 import * as orderService from "./services/orderService";
-import ManageReports from './pages/Managereports';
+import ManageReports from './pages/ManageReports';
 
 const NAVIGATION: Navigation = [
   
